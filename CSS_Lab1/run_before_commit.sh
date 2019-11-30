fcaa2ef6d199c5ed70a04cd70fe17b1e2d3b3535
CN-GuoZiyang <guoziyang0033@gmail.com>
2019-11-30 13:04:05 +0800

    增加了一些脚本

---
+++ CSS_Lab1/run_before_commit.sh
@@ -0,0 +1,15 @@
+#bin/sh
+
+echo "请在源文件所在目录执行此脚本"
+echo "获取root密码"
+sudo echo "获取成功"
+
+sudo rm *.exe
+sudo rm ./echo
+sudo rm ./fork
+sudo rm ./httpService
+sudo rm ./kill
+sudo rm ./main1
+sudo rm ./setuid
+
+echo "清除可执行文件完成"
