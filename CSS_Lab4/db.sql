bc00898e23be334515d0c9c8fc21293ee700e504
CN-GuoZiyang <guoziyang0033@gmail.com>
2019-12-21 11:41:16 +0800

    完成Lab4的SQL

--- CSS_Lab4/db.sql
+++ CSS_Lab4/db.sql
@@ -1,27 +1,80 @@
-DROP DATABASE IF EXISTS lab3;
-CREATE DATABASE `lab3` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE lab3;
+DROP DATABASE IF EXISTS lab4;
+CREATE DATABASE `lab4` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE lab4;
 
-CREATE TABLE `bank`
+CREATE TABLE `bankuser`
 (
-    id int PRIMARY KEY AUTO_INCREMENT,
+    id int PRIMARY KEY,
     username varchar(255) UNIQUE,
-    currency int NOT NULL,
     valid tinyint(1) NOT NULL
 );
 
-INSERT INTO `bank` VALUES (1, 'ziyang', 100, true);
-INSERT INTO `bank` VALUES (2, 'exp', 30, true);
+CREATE TABLE `bankcurrency`
+(
+    id int PRIMARY KEY,
+    currency int NOT NULL,
+    FOREIGN KEY(id) REFERENCES bankuser(id)
+);
 
+CREATE TABLE `bankidentity`
+(
+    id int PRIMARY KEY,
+    isadmin tinyint(1) NOT NULL,
+    FOREIGN KEY(id) REFERENCES bankuser(id)
+);
+
+INSERT INTO `bankuser` VALUES (1, 'admin', true);
+INSERT INTO `bankuser` VALUES (2, 'ziyang', true);
+INSERT INTO `bankuser` VALUES (3, 'exp', true);
+
+INSERT INTO `bankcurrency` VALUES (2, 100);
+INSERT INTO `bankcurrency` VALUES (3, 40);
+
+INSERT INTO `bankidentity` VALUES (1, 1);
+INSERT INTO `bankidentity` VALUES (2, 0);
+INSERT INTO `bankidentity` VALUES (3, 0);
+
+DROP USER if EXISTS 'admin'@'localhost';
 DROP USER if EXISTS 'ziyang'@'localhost';
 DROP USER if EXISTS 'exp'@'localhost';
 
-FLUSH PRIVILEGES;
-
+CREATE USER 'admin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
 CREATE USER 'ziyang'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ziyang';
 CREATE USER 'exp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'exp';
 
-GRANT select(currency), select(username), select(valid) ON lab3.bank TO 'ziyang'@'localhost';
-GRANT select(currency), select(username), select(valid) ON lab3.bank TO 'exp'@'localhost';
+GRANT select ON lab4.bankuser TO 'admin'@'localhost';
+GRANT select(id), select(currency), update(currency) ON lab4.bankcurrency TO 'admin'@'localhost';
+GRANT select ON lab4.bankidentity TO 'admin'@'localhost';
+
+GRANT select ON lab4.bankuser TO 'ziyang'@'localhost';
+GRANT select ON lab4.bankcurrency TO 'ziyang'@'localhost';
+GRANT select ON lab4.bankidentity TO 'ziyang'@'localhost';
+
+GRANT select ON lab4.bankuser TO 'exp'@'localhost';
+GRANT select ON lab4.bankcurrency TO 'exp'@'localhost';
+GRANT select ON lab4.bankidentity TO 'exp'@'localhost';
+
+DROP PROCEDURE IF EXISTS login;
+DELIMITER $$
+CREATE PROCEDURE login(IN p_username varchar(255), OUT p_isadmin tinyint(1), OUT p_currency int)
+BEGIN
+    DECLARE p_id int;
+    SELECT id INTO p_id FROM lab4.bankuser WHERE username=p_username;
+    SELECT isadmin INTO p_isadmin FROM lab4.bankidentity WHERE id=p_id;
+    SELECT currency INTO p_currency FROM lab4.bankcurrency WHERE id=p_id;
+END$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS queryalluser;
+DELIMITER $$
+CREATE PROCEDURE queryalluser()
+BEGIN
+    SELECT lab4.bankuser.id, lab4.bankuser.username, lab4.bankcurrency.currency 
+    FROM lab4.bankuser, lab4.bankidentity, lab4.bankcurrency 
+    WHERE lab4.bankuser.id = lab4.bankcurrency.id 
+    AND lab4.bankuser.id = lab4.bankidentity.id 
+    AND lab4.bankidentity.isadmin = 0;
+END$$
+DELIMITER ;
 
 FLUSH PRIVILEGES;
